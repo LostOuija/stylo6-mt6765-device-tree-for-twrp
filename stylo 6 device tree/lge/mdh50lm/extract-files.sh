b64aa0ec3d7e18f6c428ded28485e46548b02ae9
LostOuija <106669288+LostOuija@users.noreply.github.com>
2024-07-19 14:53:25 -0700

    Update extract-files.sh

--- stylo 6 device tree/lge/mdh50lm/extract-files.sh
+++ stylo 6 device tree/lge/mdh50lm/extract-files.sh
@@ -58,4 +58,4 @@ setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
 
 extract "${MY_DIR}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
 
-"${MY_DIR}/setup-makefiles.sh"￼Enter
+"${MY_DIR}/setup-makefiles.sh"
